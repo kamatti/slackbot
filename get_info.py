550f939e471af7023d6dbae22a1f5026e1a00aae
Tomomu Kamachi <s1300712@sendai-nct.jp>
2016-12-30 15:46:36 +0900

    get_info.pyをモジュール化

--- get_info.py
+++ get_info.py
@@ -2,29 +2,38 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import csv
+from datetime import datetime as dt
 
-html = urlopen('http://hirose.sendai-nct.ac.jp/kyuko/kyuko.cgi')
-soup = BeautifulSoup(html, 'html.parser')
 
-tables = soup.findAll('table', {'width': '650'})
+def get_info(filename):
+    html = urlopen('http://hirose.sendai-nct.ac.jp/kyuko/kyuko.cgi')
+    soup = BeautifulSoup(html, 'html.parser')
 
-csvFile = open('change_class.csv', 'wt', newline='', encoding='utf-8')
-writer = csv.writer(csvFile)
+    # 変更情報はtable width=650に記載されている
+    tables = soup.findAll('table', {'width': '650'})
 
-try:
-    for row in tables:
-        info = []
-        # 日付，クラス，時間を取得
-        for cell in row.findAll('b'):
-            info.append(cell.get_text())
-        # 教員名を取得(fontタグで指定)
-        info.append(row.find('font', {'color': '#00008B'}).get_text().rstrip())
-        # 変更の状態(休講，変更)を画像イメージのsrcから判断
-        img = row.find('img')['src'][6:]
-        info.append(img)
-        # 教科名を取得
-        info.append(row.find('table').find('font').get_text())
-        # 取得したデータを書き込む
-        writer.writerow(info)
-finally:
-    csvFile.close()
+    #filename = dt.now().strftime('%Y%m%d%H') + '_change_info.csv'
+    csvFile = open(filename, 'wt', newline='', encoding='utf-8')
+    writer = csv.writer(csvFile)
+
+    try:
+        for row in tables:
+            info = []
+            # 日付，クラス，時間を取得
+            for cell in row.findAll('b'):
+                info.append(cell.get_text())
+            # 教員名を取得(fontタグで指定)
+            info.append(row.find('font', {'color': '#00008B'}).get_text().rstrip())
+            # 変更の状態(休講，変更)を取得(画像のsrcタグで判断)
+            img = row.find('img')['src'][6:]
+            info.append(img)
+            # 教科名を取得
+            info.append(row.find('table').find('font').get_text())
+            # 取得したデータを書き込む
+            writer.writerow(info)
+    finally:
+        csvFile.close()
+
+if __name__ == '__main__':
+    filename = dt.now().strftime('%Y%m%d%H') + '_change_info.csv'
+    get_info(filename)
