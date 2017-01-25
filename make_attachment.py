c7beaa7920b92ce7e3b3f4496f81cd62521d4864
Tomomu Kamachi <s1300712@sendai-nct.jp>
2017-01-25 18:01:25 +0900

    正規表現をパラメータで指定するよう変更

--- make_attachment.py
+++ make_attachment.py
@@ -2,44 +2,58 @@
 import csv
 import re
 import copy
+from datetime import datetime as dt
 
-def make_attachment(csvFile):
+
+def make_attachment(csvFile, pattern):
     """
     授業変更通知用のattachmentを生成
     ディクショナリをリストでまとめて返す
     """
-    f = open(csvFile, 'rt')
-    reader = csv.reader(f)
+    with open(csvFile, 'rt') as f:
+        reader = csv.reader(f)
 
-    # 返り値にするためのリスト
-    attachments = []
+        # 返り値にするためのリスト
+        attachments = []
 
-    attachment = {
-        'title': '授業変更情報',
-        'title_link': 'http://hirose.sendai-nct.ac.jp/kyuko/',
-        'fallback': '授業変更情報',
-        'color': '#FF0000',
-        'image_url': '',
-        'text': '',
-    }
+        attachment = {
+            'title': '',
+            'title_link': 'http://hirose.sendai-nct.ac.jp/kyuko/',
+            'fallback': '',
+            'color': '',
+            'image_url': '',
+            'text': '',
+        }
 
-    # クソみたいなあれに対応するためのゴリラみたいな正規表現
-    pattern = '\A([4INA]|[４ＩＮＡ全]).?.?[^1-35]([4LN]|[４ＬＮ全])\Z'
-    for row in reader:
-        attachment['text'] = ''
-        # 高専のHPの画像そのまま使う
-        # TODO:どうにかローカルに保存された画像を使えないか
-        attachment['image_url'] = 'http://hirose.sendai-nct.ac.jp/kyuko/img/'
-        if re.match(pattern, row[1]):
-            attachment['text'] += row[0] + '\n'
-            attachment['text'] += row[2] + '\n'
-            attachment['text'] += row[5]
-            attachment['image_url'] += row[4]
-            attachments.append(copy.deepcopy(attachment))
-
-    f.close()
+        # クソみたいなあれに対応するためのゴリラみたいな正規表現
+        # pattern = '\A([4INA]|[４ＩＮＡ全]).?.?[^1-35]([4LN]|[４ＬＮ全])\Z'
+        for row in reader:
+            attachment['text'] = ''
+            # 高専のHPの画像そのまま使う
+            # TODO:どうにかローカルに保存された画像を使えないか
+            attachment['image_url'] = 'http://hirose.sendai-nct.ac.jp/kyuko/img/'
+            if re.match(pattern, row[1]):
+                attachment['text'] += row[0] + '\n'     # 日付
+                attachment['text'] += row[2] + '\n'     # 時限
+                attachment['text'] += row[5]            # 教科
+                attachment['image_url'] += row[4]       # 画像のurl
+                # 通知で変更情報か休講情報か
+                if re.match('henko', row[4]):
+                    attachment['fallback'] = '変更情報'
+                    attachment['color'] = '#00FF00'
+                elif re.match('kyuko', row[4]):
+                    attachment['fallback'] = '休講情報'
+                    attachment['color'] = '#FF0000'
+                else:
+                    # 登録側のシステムが変わらない限りここは通らない
+                    attachment['fallback'] = '管理者に連絡してください'
+                attachments.append(copy.deepcopy(attachment))
 
     return attachments
 
 if __name__ == "__main__":
-    make_attachment('2016122921_change_info.csv')
+    filename = dt.now().strftime('%Y%m%d') + '_change_info.csv'
+    attachments = make_attachment(filename)
+
+    for e in attachments:
+        print(e)
