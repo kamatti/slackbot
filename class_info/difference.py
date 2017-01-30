87b610df81ccf4d6f1a225b36f4b6ad7cce48539
Tomomu Kamachi <s1300712@sendai-nct.jp>
2017-01-30 18:36:18 +0900

    ２ファイルの差分のリストを生成

---
+++ class_info/difference.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+from datetime import datetime as dt
+
+
+def difference(latest, oldest):
+    '''
+    ２つのファイルからlatestにある行のリストを作る
+    '''
+    with open(latest, 'r') as f:
+        set1 = set(f.readlines())
+    with open(oldest, 'r') as f:
+        set2 = set(f.readlines())
+
+    return sorted(list(set1.difference(set2)))
+
+if __name__ == '__main__':
+    diff = difference('./changes/latest.csv', './changes/oldest.csv')
+
+    with open('./changes/diff.csv', 'w') as f:
+        # f.writelines(sorted(diff))
+        f.writelines(diff)
