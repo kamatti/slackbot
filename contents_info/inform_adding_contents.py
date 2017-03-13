a17ace215b015dc6f16da77ac4139b700d1821eb
Kamachi <s1300712@sendai-nct.jp>
2017-03-13 20:53:15 +0900

    修正版

--- contents_info/inform_adding_contents.py
+++ contents_info/inform_adding_contents.py
@@ -29,6 +29,19 @@ class Slack(object):
 
         return info
 
+    def post_message_to_person(self, channel, message, *, ts=None, name=None, icon=None):
+        """
+        Slackチームの任意のチャンネルにメッセージを投稿する。
+        """
+
+        #thread化するかどうか
+        if ts:
+            info = self__slacker.chat.post_message(channel, message, as_user=True, thread_ts=ts)
+        else:
+            # slack.Response classはメンバbodyにjson形式でデータ持ってる
+            info = self.__slacker.chat.post_message(channel, message, username=name, icon_emoji=icon)
+
+
 def difference(latest, oldest):
     '''
     ２つのリストからlatestにある行のリストを作る
@@ -48,7 +61,8 @@ if __name__ == "__main__":
                         help='検索するディレクトリパス．絶対パスで指定した方が幸せです．')
     parser.add_argument('-f', '--file', action='store', type=str,
                         help='以前のデータを保存したファイルを指定．絶対パスで指定した方が幸せです．')
-    parser.add_argument('-type', action='store', choices=['f', 'd'])
+    parser.add_argument('-type', action='store', choices=['f', 'd'],
+                        help='検索する対象を指定する．指定がなければどちらも検索する．')
     args = parser.parse_args()
 
     slack = Slack(args.access_token)
@@ -64,21 +78,26 @@ if __name__ == "__main__":
 
     # 実行するコマンド
     if args.type == 'f' or args.type == 'd':
-        command = 'find {0} -type {1} | grep -v ".AppleDouble"'.format(target, args.type)
+        command = 'find {0} -type {1} | grep -v ".AppleDouble" | grep -v ".DS_Store"'.format(target, args.type)
     else:
-        command = 'find {0} | grep -v ".AppleDouble"'.format(target, args.type)
+        command = 'find {0} | grep -v ".AppleDouble" | grep -v ".DS_Store"'.format(target)
 
     proc = sb.Popen(
            command,
            shell=True,
-           stdout=sb.PIPE
+           stdin=sb.PIPE,
+           stdout=sb.PIPE,
+           stderr=sb.PIPE
     )
-    latest = proc.communicate()
+    latest, err = proc.communicate()
+    latest = latest.decode('utf-8').split('\n')
 
     diff = difference(latest, oldest)
     # 差分があれば投稿
     if len(diff):
-        slack.post_message_to_channel(args.channel, name + "が更新されました", name='諜報部', icon=':nerv:')
+        slack.post_message_to_channel(args.channel, name.rstrip('/').split('/')[-1:][0] + "が更新されました", name='諜報部', icon=':nerv:')
+        for raw in diff:
+            slack.post_message_to_person('D1L0FHJCA', raw.split('\n')[-1:][0] + " が追加されました")
 
     # ファイル内のリストを更新
     with open(name, mode='wt', encoding='utf-8') as f:
