9eb4508c534b8b71864b2d7e12657452ec06eb97
matti <s1300712@sendai-nct.jp>
2017-09-07 23:28:41 +0900

    途中経過保存

--- contents_info/inform_diff.py
+++ contents_info/inform_diff.py
@@ -1,9 +1,23 @@
 # -*- coding:utf-8 -*-
 
+import os
+import argparse
+import json
+
 import subprocess as sp
 import shlex
 
-def main():
+
+def parse_json(path):
+  with open(path, "rt") as f:
+    data = json.load(f)
+  return data
+
+def main(**args):
+  print(args)
+  settings = parse_json(args['f'])
+  print(settings)
+
   command = "git status -s"
   # (左辺).stdout.read()で出力内容を得る
   # ただし型は<bytes>で末尾に\nを含む
@@ -22,4 +36,9 @@ def main():
   print(added_files)
 
 if __name__ == "__main__":
-  main()
+  parser = argparse.ArgumentParser(description="ファイル監視スクリプト")
+  parser.add_argument("-p", type=str, help="target path", required=True)
+  parser.add_argument("-f", type=str, help="setting file(.json)", required=True)
+
+  args = parser.parse_args()
+  main(path=args.p, f=args.f)
